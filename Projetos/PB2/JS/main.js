650f78db2167bcbbd3e26e09d0def19d65a62a23
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-08-01 00:55:39 -0300

    AW1 mobile

--- Projetos/PB2/JS/main.js
+++ Projetos/PB2/JS/main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
     if (window.scrollY >= navHeight) {
         // scroll é maior que a altura do header
         header.classList.add('scroll')
@@ -29,4 +29,40 @@ window.addEventListener('scroll', function () {
         // menor que a altura do header
         header.classList.remove('scroll')
     }
+}
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .botao,
+    #about .image, #about .text, #about .botao,
+    #projects .text, #projects .swiper-container,
+    #skills .text, #skills .card,
+    #contact .text, #contact .title, #contact p, #contact .carta,
+    #form .text, #form .container,
+    footer #linha, footer .descricao`,
+    { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.backToTop')
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
 })
